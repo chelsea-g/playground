237e1b23bbb281ff7123882ff45d4c9327d2a011
chelseag <chelseagreger@outlook.com>
2018-03-11 18:31:25 -0500

    Initial Commit

---
+++ src/test/groovy/com/chelsea/codes/playground/PlaygroundApplicationTests.groovy
@@ -0,0 +1,16 @@
+package com.chelsea.codes.playground
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner)
+@SpringBootTest
+class PlaygroundApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
