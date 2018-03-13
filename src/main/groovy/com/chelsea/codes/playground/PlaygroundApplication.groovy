5d32db6d4041492ba9d76053df922ceb2ede72ae
chelseag <chelseagreger@outlook.com>
2018-03-12 21:04:51 -0500

    Initialize playground tech

---
+++ src/main/groovy/com/chelsea/codes/playground/PlaygroundApplication.groovy
@@ -0,0 +1,12 @@
+package com.chelsea.codes.playground
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class PlaygroundApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run PlaygroundApplication, args
+	}
+}
